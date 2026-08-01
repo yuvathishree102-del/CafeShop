5a9908c8574ef25ecd4f8c31416c55e59b1c18a4
yuvathishree102-del <yuvathishree102@gmail.com>
2026-08-01 20:56:21 +0530

    Add CafeShop website + images

---
+++ script.js
@@ -0,0 +1,243 @@
+const menu={
+
+coffee:[
+
+{name:"Cappuccino",price:"₹180",image:"images/cappuccino.jpg"},
+
+{name:"Corretto",price:"₹220",image:"images/corretto.jpg"},
+
+{name:"Irish Coffee",price:"₹250",image:"images/irishcoffee.jpg"},
+
+{name:"Americano",price:"₹170",image:"images/americano.jpg"},
+
+{name:"Latte",price:"₹200",image:"images/latte.jpg"},
+
+{name:"Vienna Coffee",price:"₹230",image:"images/vienna.jpg"},
+
+{name:"Espresso",price:"₹150",image:"images/espresso.jpg"},
+
+{name:"Mocha",price:"₹210",image:"images/mocha.jpg"}
+
+],
+
+coldbeverages:[
+
+{name:"Cold Coffee",price:"₹149",image:"images/coldcoffee.jpg"},
+
+{name:"Iced Latte",price:"₹169",image:"images/icedlatte.jpg"},
+
+{name:"Iced Mocha",price:"₹179",image:"images/icedmocha.jpg"},
+
+{name:"Chocolate Milkshake",price:"₹199",image:"images/chocolatemilkshake.jpg"},
+
+{name:"Vanilla Milkshake",price:"₹179",image:"images/vanillamilkshake.jpg"},
+
+{name:"Oreo Milkshake",price:"₹219",image:"images/oreomilkshake.jpg"},
+
+{name:"Strawberry Milkshake",price:"₹199",image:"images/strawberrymilkshake.jpg"},
+
+{name:"Virgin Mojito",price:"₹159",image:"images/virginmojito.jpg"}
+
+],
+
+juice:[
+
+{name:"Apple Juice",price:"₹109",image:"images/apple.jpg"},
+
+{name:"Orange Juice",price:"₹99",image:"images/orange.jpg"},
+
+{name:"Mango Juice",price:"₹119",image:"images/mango.jpg"},
+
+{name:"Watermelon Juice",price:"₹89",image:"images/watermelon.jpg"},
+
+{name:"Pineapple Juice",price:"₹99",image:"images/pineapple.jpg"},
+
+{name:"Lemon Mint Juice",price:"₹89",image:"images/lemonmint.jpg"},
+
+{name:"Pomegranate Juice",price:"₹129",image:"images/pomegranate.jpg"},
+
+{name:"Mixed Fruit Juice",price:"₹129",image:"images/mixedfruit.jpg"}
+
+],
+
+sandwich:[
+
+{name:"Veg Sandwich",price:"₹150",image:"images/vegsandwich.jpg"},
+
+{name:"Cheese Sandwich",price:"₹180",image:"images/cheesesandwich.jpg"},
+
+{name:"Club Sandwich",price:"₹220",image:"images/clubsandwich.jpg"},
+
+{name:"Paneer Sandwich",price:"₹230",image:"images/paneersandwich.jpg"},
+
+{name:"Corn & Cheese Sandwich",price:"₹159",image:"images/corn&cheesesandwich.jpg"},
+
+{name:"Mushroom Sandwich",price:"₹169",image:"images/mushroomsandwich.jpg"},
+
+{name:"Veg Mayo Sandwich",price:"₹149",image:"images/vegmayosandwich.jpg"},
+
+{name:"Spicy Mexican Sandwich",price:"₹179",image:"images/spicymexicansandwich.jpg"}
+
+],
+
+burger:[
+
+{name:"Veg Burger",price:"₹129",image:"images/vegburger.jpg"},
+
+{name:"Cheese Burger",price:"₹149",image:"images/cheeseburger.jpg"},
+
+{name:"Paneer Burger",price:"₹169",image:"images/paneerburger.jpg"},
+
+{name:"Mexican Burger",price:"₹189",image:"images/mexicanburger.jpg"},
+
+{name:"BBQ Burger",price:"₹199",image:"images/bbqburger.jpg"},
+
+{name:"Loaded Veg Burger",price:"₹229",image:"images/loadedvegburger.jpg"},
+
+{name:"Mushroom Burger",price:"₹189",image:"images/mushroomburger.jpg"},
+
+{name:"Double Cheese Burger",price:"₹219",image:"images/doublecheeseburger.jpg"}
+
+],
+
+fries:[
+
+{name:"Classic Fries",price:"₹100",image:"images/classicfries.jpg"},
+
+{name:"Cheese Fries",price:"₹160",image:"images/cheesefries.jpg"},
+
+{name:"Peri Peri Fries",price:"₹140",image:"images/periperi.jpg"},
+
+{name:"Loaded Fries",price:"₹179",image:"images/loadedfries.jpg"},
+
+{name:"BBQ Fries",price:"₹159",image:"images/bbq.jpg"},
+
+{name:"Masala Fries",price:"₹129",image:"images/masalafries.jpg"},
+
+{name:"Nacho Cheese Fries",price:"₹179",image:"images/nacho.jpg"},
+
+{name:"Mexican Fries",price:"₹169",image:"images/mexicanfries.jpg"}
+
+],
+
+pizza:[
+
+{name:"Margherita",price:"₹280",image:"images/margherita.jpg"},
+
+{name:"Farmhouse",price:"₹350",image:"images/farmhouse.jpg"},
+
+{name:"Paneer Pizza",price:"₹400",image:"images/paneerpizza.jpg"},
+
+{name:"Cheese Burst",price:"₹450",image:"images/cheeseburst.jpg"},
+
+{name:"Corn & Cheese Pizza",price:"₹329",image:"images/corn&cheesepizza.jpg"},
+
+{name:"Chicken Pepperoni Pizza",price:"₹499",image:"images/chickenpepperonipizza.jpg"},
+
+{name:"Chicken Supreme Pizza",price:"₹529",image:"images/chickensupremepizza.jpg"},
+
+{name:"Peri Peri Chicken Pizza",price:"₹519",image:"images/periperichickenpizza.jpg"}
+
+],
+
+waffle:[
+
+{name:"Classic Waffle",price:"₹169",image:"images/classicwaffle.jpg"},
+
+{name:"Nutella Waffle",price:"₹249",image:"images/nutellawaffle.jpg"},
+
+{name:"Blueberry Waffle",price:"₹239",image:"images/blueberrywaffle.jpg"},
+
+{name:"Banana Caramel Waffle",price:"₹249",image:"images/bananacaramelwaffle.jpg"},
+
+{name:"Oreo Waffle",price:"₹239",image:"images/oreowaffle.jpg"},
+
+{name:"Strawberry Waffle",price:"₹229",image:"images/strawberrywaffle.jpg"},
+
+{name:"Belgian Waffle",price:"₹199",image:"images/belgianwaffle.jpg"},
+
+{name:"Mixed Berry Waffle",price:"₹259",image:"images/mixedberrywaffle.jpg"}
+
+],
+
+cakes:[
+
+{name:"Chocolate Cake",price:"₹180",image:"images/chocolatecake.jpg"},
+
+{name:"Black Forest",price:"₹200",image:"images/blackforest.jpg"},
+
+{name:"Red Velvet",price:"₹230",image:"images/redvelvet.jpg"},
+
+{name:"Butterscotch",price:"₹190",image:"images/butterscotch.jpg"},
+
+{name:"Chocolate Brownie",price:"₹160",image:"images/chocolatebrownie.jpg"},
+
+{name:"Bluberry Cheese Cake",price:"₹240",image:"images/blueberrycheesecake.jpg"},
+
+{name:"Chocolate Muffin",price:"₹120",image:"images/chocolatemuffin.jpg"},
+
+{name:"Glazed Donut",price:"₹80",image:"images/glazeddonut.jpg"}
+
+],
+
+icecreams:[
+
+{name:"Vanilla Ice Cream",price:"₹99",image:"images/vanillaicecream.jpg"},
+
+{name:"Chocolate Ice Cream",price:"₹109",image:"images/chocolateicecream.jpg"},
+
+{name:"Strawberry Ice Cream",price:"₹109",image:"images/strawberryicecream.jpg"},
+
+{name:"Butterscotch Ice Cream",price:"₹119",image:"images/butterscotchicecream.jpg"},
+
+{name:"Black Currant Ice Cream",price:"₹129",image:"images/blackcurranticecream.jpg"},
+
+{name:"Cookies & Cream Ice Cream",price:"₹149",image:"images/cookies&creamicecream.jpg"},
+
+{name:"Brownie with Ice Cream",price:"₹199",image:"images/browniewithicecream.jpg"},
+
+{name:"Triple Scoop Ice Cream",price:"₹229",image:"images/triplescoopicecream.jpg"}
+
+],
+
+salad:[
+
+{name:"Green Garden Salad",price:"₹149",image:"images/greengardensalad.jpg"},
+
+{name:"Fruit Salad",price:"₹169",image:"images/fruitsalad.jpg"},
+
+{name:"Chicken Wrap",price:"₹249",image:"images/chickenwrap.jpg"},
+
+{name:"Greek Salad",price:"₹189",image:"images/greeksalad.jpg"},
+
+{name:"Caesar Salad",price:"₹199",image:"images/caesarsalad.jpg"},
+
+{name:"Paneer Tikka Wrap",price:"₹229",image:"images/paneertikkawrap.jpg"},
+
+{name:"Peri Peri Chicken Wrap",price:"₹269",image:"images/periperichickenwrap.jpg"},
+
+{name:"Falafel Wrap",price:"₹219",image:"images/falafelwrap.jpg"}
+
+]
+
+};
+
+function showCategory(category) {
+
+    let output = "";
+
+    menu[category].forEach(item => {
+
+        output += `
+        <div class="card">
+            <img src="${item.image}" alt="${item.name}">
+            <h3>${item.name}</h3>
+            <p>${item.price}</p>
+            <button>Add to Cart</button>
+        </div>
+        `;
+
+    });
+
+    document.getElementById("items").innerHTML = output;
+}
